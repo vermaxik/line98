a30f20ff21e171d4228bcf33c837325c0604f3ae
Maksym Verbovyi <maksym.verbovyi@wlw.de>
2019-10-14 18:19:49 +0200

    refactoring

--- lib/line98/game.ex
+++ lib/line98/game.ex
@@ -24,44 +24,65 @@ defmodule Line98.Game do
   end
 
   def handle_call({:move_ball, position}, _from, state) do
-    {:reply, grow_ball(state, position), state}
+    new_state = grow_ball(state, position)
+    {:reply, new_state, new_state}
   end
 
   defp generate_board() do
-    Enum.to_list(1..100)
-      |>  Enum.reduce([], fn(value, acc) -> List.insert_at(acc, -1, %{cell: value, color: "empty", type: "ball"}) end)
+    # Enum.to_list(1..100)
+    # |> Enum.reduce([], fn value, acc ->
+    #  List.insert_at(acc, -1, %{cell: value, color: "empty", type: "ball"})
+    # end)
+
+    # for n <- 1..100, do: %{cell: n, color: "empty", type: "ball"}
+
+    1..100
+    |> Enum.reduce([], fn value, acc ->
+      [%{cell: value, color: "empty", type: "ball"} | acc]
+    end)
+    |> Enum.reverse()
   end
 
   defp random_color() do
-    @ballColors |> Enum.shuffle |> List.first
+    @ballColors |> Enum.shuffle() |> List.first()
   end
 
   defp random_cell() do
-    Enum.to_list(1..100) |> Enum.shuffle |> List.first
+    Enum.random(1..100)
   end
 
   defp p_init_game() do
-    board = generate_board()
-
-    update_board(board, "ball")
-      |> update_board("ball")
-      |> update_board("ball")
-      |> update_board("ball")
-      |> update_board("ball")
-      |> update_board("dot")
-      |> update_board("dot")
-      |> update_board("dot")
+    generate_board()
+    |> update_board("ball", 5)
+    |> update_board("dot", 3)
   end
 
-  defp update_board(board, type) do
-    cell = random_cell()
-    List.update_at(board, cell, fn _ -> %{cell: cell, color: random_color(), type: type} end)
+  def update_board(board, type, times) do
+    random_cells = 1..100 |> Enum.shuffle() |> Enum.take(times)
+
+    board
+    |> Enum.map(fn %{cell: cell} = item ->
+      cond do # case https://elixir-lang.org/getting-started/case-cond-and-if.html
+        Enum.member?(random_cells, cell) ->
+          %{item | color: random_color(), type: type}
+
+        true ->
+          item
+      end
+    end)
+
+    #
+    # List.update_at(board, cell, fn _ -> %{cell: cell, color: random_color(), type: type} end)
   end
 
   defp grow_ball(board, position) do
-    board |> Enum.reduce([], fn(item, acc) ->
-      updated_item = if item.type == "dot", do: Map.update!(item, :type, fn _ -> "ball" end), else: item
+    board
+    |> Enum.reduce([], fn item, acc ->
+      updated_item =
+        if item.type == "dot", do: Map.update!(item, :type, fn _ -> "ball" end), else: item
+
       List.insert_at(acc, -1, updated_item)
     end)
+    |> update_board("dot", 3)
   end
 end
--- lib/line98_web/live/play_live.ex
+++ lib/line98_web/live/play_live.ex
@@ -7,12 +7,17 @@ defmodule Line98Web.PlayLive do
   end
 
   def mount(_session, socket) do
-    {:ok, assign(socket, board: Line98.Game.board(), selected_item: nil) }
+    IO.inspect(Line98.Game.board())
+    {:ok, assign(socket, board: Line98.Game.board(), selected_item: nil)}
   end
 
   def handle_event("range", %{"select" => select_value}, socket) do
-    IO.inspect select_value
-    {:noreply, assign(socket, board: Line98.Game.move_ball(select_value |> String.to_integer), selected_item: select_value )}
-  end
+    IO.inspect(select_value)
 
+    {:noreply,
+     assign(socket,
+       board: Line98.Game.move_ball(select_value |> String.to_integer()),
+       selected_item: select_value
+     )}
+  end
 end
